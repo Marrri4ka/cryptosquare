6da747f4fc4f23121ac59389b15bf51364fff38b
Mariia Stastuk and Svitlana Filatova <mariia@>
2019-04-04 15:48:26 -0700

    add js logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,32 @@ $(document).ready(function(){
   $("form#formOne").submit(function(event){
     event.preventDefault();
 
-  // var sentence = $("input#userInput").val();
-  // var characters = sentence.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
-  // var letters = characters.split("");
-  let cat = ["c","a","t","y"];
-  let dog = ["d","o","g","y"];
- let pet = [
-   ["c","a","t","y"],
-   ["d","o","g","y"]
- ];
- for (let i =0; i<2; i++){
-   console.log("Row #" + i) for (let j = 0; j < 4; j++) {
-    console.log(i, j, pet[i][j])
-
- }
+  var sentence = $("input#userInput").val();
+  var characters = sentence.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
+  var letters = characters.split("");
+  var k = letters.length;
+  var rec= Math.sqrt(k);
+var x = new Array(rec);
+for (var i=0; i<x.length; i++){
+  x[i]= new Array(rec);
 }
+console.log(x);
+  // var rectangle = Math.pow(k);
+  let monday = ["m", "o", "n", "d"];
+  let tuesday = ["t", "u", "e", "s"];
 
+  let temps = [
+     ["c", "a", "t", "y"],
+     ["d", "o", "g", "y"],
+
+  ];
+
+  for (let i = 0; i < 2; i++) {
+     console.log("Row #" + i)
+      for (let j = 0; j < 4; j++) {
+        console.log(i, j, temps[i][j])
+     }
+  }
 
 //     console.log(letters)
 //
